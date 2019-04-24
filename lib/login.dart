df41f36763f05eb29896b4167e97bf9c3d2c4101
Keanu <keanukent.dev@gmail.com>
2019-04-24 13:05:34 +0800

    Home page and scrollable page
    Added support for scrolling in select student and login page (scrolls when in landscape mode)

--- lib/login.dart
+++ lib/login.dart
@@ -17,98 +17,102 @@ class _LoginPageState extends State<LoginPage> {
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 40.0),
           alignment: AlignmentDirectional.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextField(
-                      controller: _userController,
-                      decoration: InputDecoration(
-                        filled: false,
-                        labelText: 'Login',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5))
-                        )
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextField(
+                        controller: _userController,
+                        decoration: InputDecoration(
                           filled: false,
-                          labelText: 'Password',
+                          labelText: 'Login',
                           border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(5))
+                            borderRadius: BorderRadius.all(Radius.circular(5))
                           )
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 8.0),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: RaisedButton(
-                        child: Text(
-                          'Log In',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700
-                          ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                            filled: false,
+                            labelText: 'Password',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(5))
+                            )
                         ),
-                        onPressed: () {},
-                        elevation: 3.0,
-                        padding: EdgeInsets.symmetric(vertical: 16.0),
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                       ),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 32.0),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: RaisedButton(
-                              child: Text('Login with FB'),
-                              onPressed: () {},
-                              elevation: 0.0,
-                              padding: EdgeInsets.symmetric(vertical: 12.0),
-                              highlightElevation: 0.0,
-                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700
                             ),
                           ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          elevation: 3.0,
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                         ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 8.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: RaisedButton(
-                              child: Text('Login with Google'),
-                              onPressed: () {},
-                              elevation: 0.0,
-                              padding: EdgeInsets.symmetric(vertical: 12.0),
-                              highlightElevation: 0.0,
-                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  )
-                ],
-              )
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 32.0),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                child: Text('Login with FB'),
+                                onPressed: () {},
+                                elevation: 0.0,
+                                padding: EdgeInsets.symmetric(vertical: 12.0),
+                                highlightElevation: 0.0,
+                                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 8.0),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                child: Text('Login with Google'),
+                                onPressed: () {},
+                                elevation: 0.0,
+                                padding: EdgeInsets.symmetric(vertical: 12.0),
+                                highlightElevation: 0.0,
+                                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
 
-            ],
+              ],
+            ),
           ),
         ),
       )
