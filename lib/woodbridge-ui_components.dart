5051d5b5a6881cf0e4ec7affe0d1316e7b7e153a
Keanu <keanukent.dev@gmail.com>
2019-04-26 15:45:14 +0800

    Added attendance layout

--- lib/woodbridge-ui_components.dart
+++ lib/woodbridge-ui_components.dart
@@ -38,3 +38,48 @@ class ProfileHeader extends StatelessWidget {
     );
   }
 }
+
+class DashboardTile extends StatelessWidget {
+  final String label;
+  final String value;
+  final Widget child;
+  final bool displayPlainValue;
+
+  DashboardTile({
+    Key key,
+    this.label,
+    // ignore: avoid_init_to_null
+    this.child: null,
+    // ignore: avoid_init_to_null
+    this.value: null,
+    this.displayPlainValue: false
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            this.label,
+            style: TextStyle(
+              fontSize: 12.0,
+              color: Colors.grey[700]
+            ),
+          ),
+          displayPlainValue ? Text(
+            this.value,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              color: Colors.grey[800]
+            ),
+          ) : this.child,
+        ],
+      ),
+    );
+  }
+
+}
