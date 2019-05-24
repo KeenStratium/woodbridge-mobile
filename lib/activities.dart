37875260fdef7cbc205dc3267bab49c944768671
Keanu Kent <keanukent.dev@gmail.com>
2019-05-24 10:24:37 +0800

    Sticky header

--- lib/activities.dart
+++ lib/activities.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'woodbridge-ui_components.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
+import 'dart:ui' as ui;
 
 class ActivityEvent {
   String title;
@@ -11,7 +13,52 @@ class ActivityEvent {
   ActivityEvent({this.title, this.venue, this.time, this.day, this.weekday});
 }
 
-List<ActivityEvent> events = <ActivityEvent>[
+List<ActivityEvent> may = <ActivityEvent>[
+  ActivityEvent(
+    title: 'Event title 1',
+    venue: 'Multi-purpose Gym',
+    time: '8:00am',
+    day: '02',
+    weekday: 'Tue'
+  ),
+  ActivityEvent(
+    title: 'Event title 2',
+    venue: 'Carmelite Hall',
+    time: '9:30am',
+    day: '02',
+    weekday: 'Tue'
+  ),
+  ActivityEvent(
+    title: 'Event title 3',
+    venue: 'Multi-purpose Gym',
+    time: '8:00am',
+    day: '04',
+    weekday: 'Thu'
+  ),
+  ActivityEvent(
+    title: 'Event title 4',
+    venue: 'Multi-purpose Gym',
+    time: '1:30pm',
+    day: '04',
+    weekday: 'Fri'
+  ),
+  ActivityEvent(
+    title: 'Event title 5',
+    venue: 'Multi-purpose Gym',
+    time: '1:30pm',
+    day: '04',
+    weekday: 'Fri'
+  ),
+  ActivityEvent(
+    title: 'Event title 6',
+    venue: 'Multi-purpose Gym',
+    time: '1:30pm',
+    day: '04',
+    weekday: 'Fri'
+  ),
+];
+
+List<ActivityEvent> june = <ActivityEvent>[
   ActivityEvent(
       title: 'Event title 1',
       venue: 'Multi-purpose Gym',
@@ -56,6 +103,264 @@ List<ActivityEvent> events = <ActivityEvent>[
   ),
 ];
 
+List<ActivityEvent> july = <ActivityEvent>[
+  ActivityEvent(
+      title: 'Event title 1',
+      venue: 'Multi-purpose Gym',
+      time: '8:00am',
+      day: '02',
+      weekday: 'Tue'
+  ),
+  ActivityEvent(
+      title: 'Event title 2',
+      venue: 'Carmelite Hall',
+      time: '9:30am',
+      day: '02',
+      weekday: 'Tue'
+  ),
+  ActivityEvent(
+      title: 'Event title 3',
+      venue: 'Multi-purpose Gym',
+      time: '8:00am',
+      day: '04',
+      weekday: 'Thu'
+  ),
+  ActivityEvent(
+      title: 'Event title 4',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+  ActivityEvent(
+      title: 'Event title 5',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+  ActivityEvent(
+      title: 'Event title 6',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+];
+
+List<ActivityEvent> august = <ActivityEvent>[
+  ActivityEvent(
+      title: 'Event title 1',
+      venue: 'Multi-purpose Gym',
+      time: '8:00am',
+      day: '02',
+      weekday: 'Tue'
+  ),
+  ActivityEvent(
+      title: 'Event title 2',
+      venue: 'Carmelite Hall',
+      time: '9:30am',
+      day: '02',
+      weekday: 'Tue'
+  ),
+  ActivityEvent(
+      title: 'Event title 3',
+      venue: 'Multi-purpose Gym',
+      time: '8:00am',
+      day: '04',
+      weekday: 'Thu'
+  ),
+  ActivityEvent(
+      title: 'Event title 4',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+  ActivityEvent(
+      title: 'Event title 5',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+  ActivityEvent(
+      title: 'Event title 6',
+      venue: 'Multi-purpose Gym',
+      time: '1:30pm',
+      day: '04',
+      weekday: 'Fri'
+  ),
+];
+
+List<List<ActivityEvent>> monthActivities = <List<ActivityEvent>>[
+  may, june, july, august
+];
+
+
+List<Widget> _buildActivities (BuildContext context) {
+  List<Widget> activities = List<Widget>();
+
+  int i = 0;
+
+  activities.addAll(_buildLists(context, i, i += monthActivities.length));
+
+  return activities;
+}
+
+List<Widget> _buildLists(BuildContext context, int firstIndex, int count) {
+  return List.generate(count, (sliverIndex) {
+    sliverIndex += firstIndex;
+    return new SliverStickyHeaderBuilder(
+      builder: (context, state) => _buildHeader(context, sliverIndex, state),
+      sliver: new SliverList(
+        delegate: new SliverChildBuilderDelegate((context, i) =>
+          Container(
+            margin: EdgeInsets.only(top: i == 0 ? 20.0 : 0.00, bottom:  i == monthActivities[sliverIndex].length - 1 ? 20.00 : 0.00),
+            padding: EdgeInsets.symmetric(vertical: 8.0),
+            child: Container(
+              height: 80.0,
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(6.0)),
+                boxShadow: [BrandTheme.cardShadow],
+              ),
+              child: Flex(
+                direction: Axis.horizontal,
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            monthActivities[sliverIndex][i].day,
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              color: Theme.of(context).accentColor
+                            ),
+                          ),
+                          Text(
+                            monthActivities[sliverIndex][i].weekday,
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.black54
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          monthActivities[sliverIndex][i].title,
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w700
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.access_time,
+                                  size: 16.0,
+                                  color: Colors.black54,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 1.0),
+                                ),
+                                Text(
+                                  monthActivities[sliverIndex][i].time,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black87,
+                                    fontSize: 14.0
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 6.0),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.location_on,
+                                  size: 16.0,
+                                  color: Colors.black54,
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 1.0),
+                                ),
+                                Text(
+                                  monthActivities[sliverIndex][i].venue,
+                                  overflow: TextOverflow.ellipsis,
+                                  softWrap: true,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.black87,
+                                    fontSize: 14.0
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+                ],
+              ),
+            ),
+          ),
+          childCount: monthActivities.length,
+        ),
+      ),
+    );
+  });
+}
+
+Widget _buildHeader(BuildContext context, int index, SliverStickyHeaderState state, {String text}) {
+  return new Container(
+    height: 48.0,
+//    color: Colors.white,
+    padding: EdgeInsets.symmetric(horizontal: 20.0),
+    alignment: Alignment.centerLeft,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border(
+        bottom: BorderSide(
+          color: Colors.grey[300],
+          width: 1.0
+        ),
+        top: BorderSide(
+          color: state.isPinned ? Colors.white : Colors.grey[300],
+          width: 1.0
+        )
+      )
+    ),
+    child: new Text(
+      text ?? 'Header #${index+1}',
+      style: TextStyle(
+        color: Colors.black87,
+        fontSize: 16.0,
+        fontWeight: FontWeight.w700
+      ),
+    ),
+  );
+}
+
 class Activities extends StatelessWidget {
   final String firstName;
   final String lastName;
@@ -79,91 +384,12 @@ class Activities extends StatelessWidget {
               firstName: this.firstName,
               lastName: this.lastName,
             ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Flex(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 12.0),
-                      alignment: AlignmentDirectional.centerStart,
-                      child: Text(
-                        'January',
-                        style: TextStyle(
-                            fontSize: 18.0,
-                          fontWeight: FontWeight.w600
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        itemCount: events.length,
-                        itemExtent: 96.0,
-                        cacheExtent: 96.0,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                            padding: EdgeInsets.symmetric(vertical: 8.0),
-                            child: Container(
-                              height: 80.0,
-                              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.grey[200],
-                                  width: 1.00
-                                )
-                              ),
-                              child: Flex(
-                                direction: Axis.horizontal,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 24.0),
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          events[index].day,
-                                          style: TextStyle(
-                                            fontSize: 24.0
-                                          ),
-                                        ),
-                                        Text(events[index].weekday,)
-                                      ],
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: <Widget>[
-                                        Text(
-                                          events[index].title,
-                                          style: TextStyle(
-                                            fontSize: 18.0,
-                                            fontWeight: FontWeight.w500
-                                          ),
-                                        ),
-                                        Row(
-                                          children: <Widget>[
-                                            Text(events[index].time),
-                                            Text('/'),
-                                            Text(events[index].venue)
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                )
-              ),
+            Flexible(
+              child: Builder(builder: (BuildContext context) {
+                return new CustomScrollView(
+                  slivers: _buildActivities(context),
+                );
+              }),
             )
           ],
         ),
