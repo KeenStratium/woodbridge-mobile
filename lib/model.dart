850677c5cff2de998e2dd3b2860f06c0c59cbccb
Keanu Kent B. Gargar <keanukent.dev@gmail.com>
2019-07-08 10:48:37 +0800

    set online config

--- lib/model.dart
+++ lib/model.dart
@@ -1,7 +1,7 @@
 Map config = {
   'protocol': 'http',
-  'serverUrl': '10.99.201.151',
-  'serverPort': '4100'
+  'serverUrl': '54.169.38.97',
+  'serverPort': '4200'
 };
 
 String baseServer = '${config['protocol']}://${config['serverUrl']}:${config['serverPort']}';
